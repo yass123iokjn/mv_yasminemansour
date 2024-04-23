3307de6284e87b510fbce3be4889ea11f1f9cc4c
yasmine mansour <yasminemansour2003@gmail.com>
2024-04-23 10:28:25 +0100

    Creation of MixController class

---
+++ src/Controller/MixController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\VinylMix;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MixController extends AbstractController
+{
+    #[Route('/mix/new')]
+    public function new(): Response
+    {
+        $mix = new VinylMix();
+        $mix->setTitle('Do you Remember... Phil Collins?!');
+        $mix->setDescription('A pure mix of drummers turned singers!');
+        $mix->setGenre('pop');
+        $mix->setTrackCount(rand(5, 20));
+        $mix->setVotes(rand(-50, 50));
+
+        dd($mix);
+    }
+}
